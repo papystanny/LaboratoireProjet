113698f92597d2f5cec4cda8f37722237d0bc525
alexm <alexm@LAPTOP-RC8U7I92>
2022-11-23 05:59:48 -0800

    Classes Projet et Employé

---
+++ LaboratoireProjet/LaboratoireProjet/Employe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoireProjet
+{
+    internal class Employe
+    {
+        int id;
+        string nom;
+        string prenom;
+
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+
+        public override string ToString()
+        { 
+            return nom + " " + prenom;
+        }
+    }
+}
