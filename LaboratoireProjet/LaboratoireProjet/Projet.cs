633329fb12c322a0aca0d120538e05604b744658
alexm <alexm@LAPTOP-RC8U7I92>
2022-11-28 17:29:10 -0800

    Push de la version de Stan

--- LaboratoireProjet/LaboratoireProjet/Projet.cs
+++ LaboratoireProjet/LaboratoireProjet/Projet.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,17 +10,25 @@ namespace LaboratoireProjet
 {
     internal class Projet
     {
-        int numProjet;
+        string numProjet;
         string dateDebut;
-        int budget;
+        double budget;
         string description;
-        int matriculeEmp;
+        string matriculeEmp;
+        string nomEmploye;
+        string prenomEmploye;
 
-        public int NumProjet { get => numProjet; set => numProjet = value; }
+        public string NumProjet { get => numProjet; set => numProjet = value; }
         public string DateDebut { get => dateDebut; set => dateDebut = value; }
-        public int Budget { get => budget; set => budget = value; }
+        public double Budget { get => budget; set => budget = value; }
         public string Description { get => description; set => description = value; }
-        public int MatriculeEmp { get => matriculeEmp; set => matriculeEmp = value; }
+        public string MatriculeEmp { get => matriculeEmp; set => matriculeEmp = value; }
+        public string NomEmploye { get => nomEmploye; set => nomEmploye = value; }
+        public string PrenomEmploye { get => prenomEmploye; set => prenomEmploye = value; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+        this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
         public override string ToString()
         {
---
+++ LaboratoireProjet/LaboratoireProjet/recherche.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace LaboratoireProjet
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class recherche : Page
+    {
+        public recherche()
+        {
+            this.InitializeComponent();
+        }
+
+        private void autoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            List<string> suggestions = new List<string>();
+
+            foreach (var texte in Gestion.)
+            {
+                if (texte.Contains(autoSuggestBox.Text))
+                    suggestions.Add(texte);
+            }
+
+            if (suggestions.Count == 0)
+                suggestions.Add("Aucun résultat");
+
+            autoSuggestBox.ItemsSource = suggestions;
+        }
+
+        private void autoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            //on traite le texte entré dans la autoSuggestBox
+        }
+
+
+    }
+}
